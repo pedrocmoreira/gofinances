63da631a73bbe49a14ee3f0f04df8a5263d86a5c
Pedro Moreira <pedrocmoreira97@gmail.com>
2021-05-27 11:55:20 -0300

    Add estilizaçao do dashboard

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import {
+    Container,
+    Header,
+
+} from './styles';
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Header>
+            
+            </Header>
+        </Container>
+    );
+}
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,14 @@
+//Aqui é onde deixo toda a estilização do styled componets
+import styled from 'styled-components/native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+ 
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    background-color: ${({theme}) => theme.colors.primary};
+`;
