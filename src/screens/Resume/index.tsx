16b1d2595665ec656775f0942888500ee64af785
Pedro Moreira <pedrocmoreira97@gmail.com>
2021-06-11 06:12:36 -0300

    Lista para a tela de resumo pronta

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,20 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { HistoryCard } from '../../components/HistoryCard';
 
-import { Container, Header, Title,  } from './styles';
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+} from './styles';
+import { categories } from '../../utils/categories';
 
+interface TransactionData {
+    type: 'positive' | 'negative';
+    name: string;
+    amount: string;
+    category: string;
+    date: string;
+}
+
+interface CategoryData {
+    key: string;
+    name: string;
+    total: string;
+    color: string;
+}
 
 export function Resume() {
-    return(
+    const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+    async function loadData() {
+        const dataKey = '@gofinances:transactions';
+        const response = await AsyncStorage.getItem(dataKey);
+        const responseFormatted = response ? JSON.parse(response) : [];
+
+        const expensives = responseFormatted
+            .filter((expensive: TransactionData) => expensive.type === 'negative');
+
+        const totalBycategory: CategoryData[] = [];
+
+        //Foreach para não devolver um objeto, diferente do map
+        categories.forEach(category => {
+            let categorySum = 0;
+
+            expensives.forEach((expensive: TransactionData) => {
+                if (expensive.category === category.key) {
+                    categorySum += Number(expensive.amount);
+                }
+
+            });
+
+            if (categorySum > 0) {
+                const total = categorySum
+                    .toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    })
+
+                totalBycategory.push({
+                    key: category.key,
+                    name: category.name,
+                    color: category.color,
+                    total,
+                });
+            }
+        });
+
+        setTotalByCategories(totalBycategory);
+
+    }
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    return (
         <Container>
             <Header>
                 <Title>Resumo por categoria</Title>
             </Header>
-            <HistoryCard
-                title="Compras"
-                amount="150,00"
-                color= "red"
-            />
+            <Content>
+            {
+                totalByCategories.map(item => (
+                    <HistoryCard
+                    //Como são poucos items não é preciso utilizar uma Flatlist
+                        key={item.key}
+                        title={item.name}
+                        amount={item.total}
+                        color={item.color}
+                    />
+                ))
+            }
+            </Content>
         </Container>
     );
 }
--- src/screens/Resume/styles.tsx
+++ src/screens/Resume/styles.tsx
@@ -21,10 +21,7 @@ export const Title = styled.Text`
     font-size: ${RFValue(18)}px;
     color: ${({ theme }) => theme.colors.shape};
 `;
-export const Form = styled.View`
-    flex: 1;
-    justify-content: space-between;
-    width: 100%;
 
-    padding: 24px;
-`;
+export const Content = styled.ScrollView.attrs({
+    contentContainerStyle: { flex: 1, padding: 24 }
+})``;
